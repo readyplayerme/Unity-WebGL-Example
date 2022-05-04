f0298cb81d08e25c4246425eafac0357895af132
Harrison <harrisonhough4@gmail.com>
2022-05-04 11:30:58 +0300

    chore: cleanup avatar loader

--- Assets/Scripts/WebAvatarLoader.cs
+++ Assets/Scripts/WebAvatarLoader.cs
@@ -1,46 +1,34 @@
-using System.Runtime.InteropServices;
 using ReadyPlayerMe;
 using UnityEngine;
 
 public class WebAvatarLoader : MonoBehaviour
 {
-        [DllImport("__Internal")]
-        private static extern void HideReadyPlayerMeFrame();
-    
-        [SerializeField]
-        private string AvatarURL = "https://d1a370nemizbjq.cloudfront.net/5cae00e5-5622-47c7-af0d-02028fad5beb.glb";
+    private string AvatarURL = "https://d1a370nemizbjq.cloudfront.net/5cae00e5-5622-47c7-af0d-02028fad5beb.glb";
+    private AvatarLoader avatarLoader;
 
-        [SerializeField] private bool loadOnStart = true;
-        private AvatarLoader avatarLoader;
+    private void Start()
+    {
+        avatarLoader = new AvatarLoader();
+    }
 
-        private void Start()
-        {
-            avatarLoader = new AvatarLoader();
-            if (loadOnStart)
-            {
-                LoadAvatar(AvatarURL);
-            }
-        }
+    private void OnAvatarImported(GameObject avatar)
+    {
+        Debug.Log($"Avatar imported. [{Time.timeSinceLevelLoad:F2}]");
+    }
 
-        private void OnAvatarImported(GameObject avatar)
-        {
-            Debug.Log($"Avatar imported. [{Time.timeSinceLevelLoad:F2}]");
-        }
+    private void OnAvatarLoaded(GameObject avatar, AvatarMetaData metaData)
+    {
+        Debug.Log($"Avatar loaded. [{Time.timeSinceLevelLoad:F2}]\n\n{metaData}");
+    }
 
-        private void OnAvatarLoaded(GameObject avatar, AvatarMetaData metaData)
-        {
-            Debug.Log($"Avatar loaded. [{Time.timeSinceLevelLoad:F2}]\n\n{metaData}");
-        }
+    public void LoadAvatar(string avatarUrl)
+    {
+        AvatarURL = avatarUrl;
+        avatarLoader.LoadAvatar(AvatarURL, OnAvatarImported, OnAvatarLoaded);
+    }
 
-        public void LoadAvatar(string avatarUrl)
-        {
-            AvatarURL = avatarUrl;
-            avatarLoader.LoadAvatar(AvatarURL, OnAvatarImported, OnAvatarLoaded);
-        }
-
-        public void OnWebViewAvatarGenerated(string avatarUrl)
-        {
-            //WebInterface.SetIFrameVisibility(false);
-            LoadAvatar(avatarUrl);
-        }
+    public void OnWebViewAvatarGenerated(string avatarUrl)
+    {
+        LoadAvatar(avatarUrl);
+    }
 }
