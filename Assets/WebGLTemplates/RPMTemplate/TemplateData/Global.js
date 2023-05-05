11e8d666ab6fde9cddf7650051471b812bc42c87
Harrison <harrisonhough4@gmail.com>
2023-05-05 06:47:10 +0300

    chore: update for consistency

--- Assets/WebGLTemplates/RPMTemplate/TemplateData/Global.js
+++ Assets/WebGLTemplates/RPMTemplate/TemplateData/Global.js
@@ -4,5 +4,5 @@ var loadingBar = document.querySelector("#unity-loading-bar");
 var progressBarFull = document.querySelector("#unity-progress-bar-full");
 var fullscreenButton = document.querySelector("#unity-fullscreen-button");
 var warningBanner = document.querySelector("#unity-warning");
-var canvasWrapper = document.getElementById("canvas-wrap");
+var canvasWrapper = document.querySelector("#canvas-wrap");
 var unityGame;
